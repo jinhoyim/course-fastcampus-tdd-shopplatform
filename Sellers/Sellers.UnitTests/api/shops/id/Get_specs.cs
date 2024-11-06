c301b18d40881f873aaf106fd5efe70d1285360e
Jinho Yim <jinhoyim@outlook.com>
2024-11-07 01:40:55 +0900

    실습을 위한 변경 전 코드 추가

--- Sellers/Sellers.UnitTests/api/shops/id/Get_specs.cs
+++ Sellers/Sellers.UnitTests/api/shops/id/Get_specs.cs
@@ -1,10 +1,40 @@
 using System.Net.Http.Json;
+using AutoFixture.Xunit2;
 using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Sellers.api.shops.id;
 
 public class Get_specs
 {
+    [Fact]
+    public async Task Sut_does_not_expose_user_credentials()
+    {
+        // Arrange
+        SellersServer server = SellersServer.Create();
+
+        using IServiceScope scope = server.Services.CreateScope();
+        SellersDbContext context = scope.ServiceProvider.GetRequiredService<SellersDbContext>();
+        Shop shop = new()
+        {
+            Id = Guid.NewGuid(),
+            Name = $"{Guid.NewGuid()}",
+            UserId = $"{Guid.NewGuid()}",
+            PasswordHash = $"{Guid.NewGuid()}"
+        };
+        context.Shops.Add(shop);
+        await context.SaveChangesAsync();
+        
+        // Act
+        HttpClient client = server.CreateClient();
+        HttpResponseMessage response = await client.GetAsync($"api/shops/{shop.Id}");
+        Shop? actual = await response.Content.ReadFromJsonAsync<Shop>();
+        
+        // Assert
+        actual.UserId.Should().BeNull();
+        actual.PasswordHash.Should().BeNull();
+    }
+    
     [Fact]
     public async Task Sut_does_not_expose_user_information()
     {
